15e185ee5ed89a3c760fb841ac86690ec360f928
Sluis <Ted.Sluis@rabobank.nl>
2017-01-31 14:59:29 +0100

    Added search filters and verbose and detail options

--- registry_query.pl
+++ registry_query.pl
@@ -3,12 +3,53 @@ use strict;
 use diagnostics;
 use JSON;
 use Data::Dumper;
+use Getopt::Long;
+
+# https://github.com/TranceMaster86/docker-reg-gc/blob/master/docker-reg-gc.sh
 
 # set defaults
-my $DOCKER_REGISTRY="https://<your docker registry>:5000/v2";
+my $DOCKER_REGISTRY="https://<your docker registry url>:5000/v2";
 my $ACCEPT_HEADER="Accept: application/vnd.docker.distribution.manifest.v2+json";
 
-#
+# options
+my $help;
+my $docker_registry;
+my $repofilter;
+my $tagfilter;
+my $verbose;
+my $details;
+GetOptions(
+  "help!"=>\$help,
+  "url=s"=>\$docker_registry,
+  "repo=s"=>\$repofilter,
+  "tag=s"=>\$tagfilter,
+  "verbose!"=>\$verbose,
+  "details!"=>\$details
+);
+
+# help
+if ($help) {
+  print "\nHELP docker registry query\n
+Usage:
+  $0 -help                        This help text.
+  $0 -verbose                     Display detail logging.
+  $0 -details                     Display detail info.
+  $0 -url <docker registry url>   An alternative docker registry URL.
+  $0 -repo <(part of) repo name>  Filter on repository name. 
+  $0 -tag <(part of) tag name>    Filter on tag.\n";
+  exit;
+}
+
+# Docker registry URL
+if ($docker_registry) {
+  if ($docker_registry =~ /^https:\/\/([a-z0-9]+[_\-\.\/]?)+$/i) {
+    $DOCKER_REGISTRY="$docker_registry:5000/v2";
+  } elsif ($docker_registry =~ /^([a-z0-9]+[_\-\.\/]?)+$/i) {
+    $DOCKER_REGISTRY="https://$docker_registry:5000/v2";
+  }
+}
+
+# counters
 my $REPOCOUNT=0;
 my $TAGCOUNT=0;
 
@@ -18,7 +59,7 @@ sub parse_json(@){
   my $json = JSON->new;
   my $data = from_json($json_string);
   #print Dumper($data)."\n";
-  return $data;
+  return $data;  
 }
 
 # get repositories
@@ -37,7 +78,7 @@ sub get_repository_tags(@) {
   my @curl=`curl -Ls GET "${DOCKER_REGISTRY}"/"${REPOSITORY}"/tags/list`;
   foreach my $line (@curl) {
     if ($line =~ /^\{"errors"/) {
-       print "   ERROR: '$line'";
+       print "   ERROR: '$line'" if ($verbose);
        next;
     } elsif ($line =~ /^\{"name":"([^"]+)","tags":\[([^\]]+)\]}/) {
        my $name = $1;
@@ -45,17 +86,17 @@ sub get_repository_tags(@) {
        my @tags = @{ $ { parse_json($line) } { 'tags' } };
        my @tmp;
        foreach my $tag (sort @tags) {
-         if ($tag eq "latest") {
+         if ($tag eq "latest") { 
            unshift(@tmp,$tag);
            next;
          }
          push(@tmp,$tag);
        }
        return @tmp;
-    } else {
-       print "   Undefined line: '$line'";
+    } else { 
+       print "   Undefined line: '$line'" if ($verbose);
     }
-  }
+  } 
 }
 
 # get tag date
@@ -73,13 +114,13 @@ sub get_tag_created_date_and_digest(@) {
     $curl.=$line;
     if ($line =~ /"schemaVersion":\s2,/) {
       $MANIFEST = "v2";
-    } elsif ($line =~ /"digest":\s"([^\)]+)"/) {
+    } elsif ($line =~ /"digest":\s"([^\)]+)"/) { 
       $TAGBLOBDIGEST = $1;
     } elsif ($line =~ /"(v1)Compatibility":\s"\{\\"id\\":\\"([0-9a-f]+)\\"/) {
       $MANIFEST = "v1";
       $TAGBLOBDIGEST = $2;
     }
-  }
+  } 
   $curl =~ s/\n$//g;
   #print "--->>>$curl<<<---\n";
   my $ref = parse_json($curl);
@@ -94,7 +135,7 @@ sub get_tag_created_date_and_digest(@) {
     foreach my $ref_layers (@fslayers) {
       if (exists $ { $ref_layers } { 'blobSum' } ) {
         my $blobSum = $ { $ref_layers } { 'blobSum' };
-        print "blobSum---->>>$blobSum\n";
+        print "blobSum---->>>$blobSum\n" if ($details);
       }
     }
   }
@@ -104,7 +145,7 @@ sub get_tag_created_date_and_digest(@) {
     foreach my $ref_layer (@history) {
       if (exists $ { $ref_layer } { 'v1Compatibility' } ) {
         my $ref_v1Compatibility = $ { $ref_layer } { 'v1Compatibility' };
-        print "      ref_v1Compatibility===>$ref_v1Compatibility\n";
+        print "      ref_v1Compatibility===>$ref_v1Compatibility\n" if ($details);
         #my $created = $ { $ref_v1Compatibility } { 'created' };
         #print "created------->>>$created\n";
       }
@@ -117,21 +158,21 @@ sub get_tag_created_date_and_digest(@) {
     if (exists $ { $ref_config } { 'digest' } ) {
       my $SIZE = $ { $ref_config } { 'size' };
       $TAGBLOBDIGEST = $ { $ref_config } { 'digest' };
-      print "digest-->$TAGBLOBDIGEST, size-->$SIZE\n";
+      print "digest-->$TAGBLOBDIGEST, size-->$SIZE\n" if ($details);
     }
-  }
+  } 
   if (exists $ { $ref } { 'layers' } ) {
     my $ref_layers = $ { $ref } { 'layers' };
     my @layers = @ { $ref_layers };
     foreach my $ref_layers (@layers) {
       my $SIZE = $ { $ref_layers } { 'size' };
       my $TAGBLOBDIGEST = $ { $ref_layers } { 'digest' };
-      print "  +-digest----->$TAGBLOBDIGEST, size---->$SIZE\n";
-    }
+      print "  +-digest----->$TAGBLOBDIGEST, size---->$SIZE\n" if ($details);
+    }  
   }
-  #
+  # 
   if ((!$TAGBLOBDIGEST) || ($TAGBLOBDIGEST =~ /^\s*$/)) {
-    print "   REPO=$REPOSITORY, TAG=$TAG, TAGBLOBDIGEST is empty!\n";
+    print "   REPO=$REPOSITORY, TAG=$TAG, TAGBLOBDIGEST is empty!\n" if ($details);
     return;
   }
   @curl = `curl -Ls GET "${DOCKER_REGISTRY}"/"${REPOSITORY}"/blobs/"${TAGBLOBDIGEST}"`;
@@ -146,7 +187,7 @@ sub get_tag_created_date_and_digest(@) {
   foreach my $line (@curl) {
     if ($line =~ /^\s*Docker-Content-Digest:\s(sha256:[0-9a-f]+)\s*$/) {
       my $TAGDIGEST=$1;
-      printf("  %-3s %-20s %-73s %-21s \n", $MANIFEST, $TAG, $TAGDIGEST, $DATE);
+      printf("  %-3s %-20s %-73s %-21s \n", $MANIFEST, $TAG, $TAGDIGEST, $DATE) if ($details);
     }
   }
 }
@@ -154,16 +195,18 @@ sub get_tag_created_date_and_digest(@) {
 # main
 my @repos = get_repositories();
 foreach my $repo (sort @repos) {
+  next if (($repofilter) && ($repo !~ /$repofilter/));
   $REPOCOUNT++;
   print "$repo\n";
   my @tags = get_repository_tags($repo);
   foreach my $tag (@tags) {
+    next if (($tagfilter) && ($repo !~ /$tagfilter/));
     $TAGCOUNT++;
     if ((!$tag) || ($tag =~ /^\s*$/)) {
       print "   REPO $repo, TAG is empty!\n";
       next;
     }
     get_tag_created_date_and_digest($repo,$tag);
-  }
+  }  
 }
 print "REPOCOUNT=$REPOCOUNT, TAGCOUNT=$TAGCOUNT\n";
